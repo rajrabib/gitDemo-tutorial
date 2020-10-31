1e3670913573d2d01de2b79a0a3c2e00a9d2b774
rajrabib <rajrabib@gmail.com>
2020-10-31 23:13:07 +0600

    Added a new method called display to Hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(string[] args) {
-        system.out.println("Vs Code is Awesome.")
+        system.out.println("Vs Code is Awesome.");
+        greeting("HM Nayem");
+    }
+    public static void greeting (string name) {
+    system.out.println("Welcome" + name);
     }
 }
